05c019ef5c7c67030e16ce0a68d6e904d7b4383c
xHezuSx <jakubek.zakowski@gmail.com>
2023-10-09 13:29:17 +0200

    A1

---
+++ 169543/lab1/zajecia1/src/A1.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+//2,4,6,8,10,12 zadania 
+
+public class A1 {
+    public static void main(String[] args)
+    {
+        Scanner scaner = new Scanner(System.in);
+
+        System.out.println("podaj pierwszą liczbę:");
+        int x = scaner.nextInt();
+
+        System.out.println("podaj drugą liczbę:");
+        int y = scaner.nextInt();
+
+        System.out.println("wynik to: "+ (x+y));
+
+    }
+}
